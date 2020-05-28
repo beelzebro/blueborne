7fe5dd4ffce4e60db562b0843309d8e2fde00dcc
kimocoder <christian@aircrack-ng.org>
2020-05-28 09:21:23 +0000

    Migrate to python3

--- android/utils.py
+++ android/utils.py
@@ -1,15 +1,13 @@
 import struct
 
 def _reverse_dict(d):
-    return dict(map(reversed, d.items()))
+    return dict(list(map(reversed, list(d.items()))))
 
 def create_struct_funcs(format_, definition):
-    struct_format = format_ + ''.join(map(lambda field: field[1], definition))
-    keys = list(map(lambda field: field[0], definition))
-    mappers = dict(map(lambda field: (field[0], field[2]),
-                       filter(lambda field: len(field) > 2, definition)))
-    reverse_mappers = dict(map(lambda item: (item[0], _reverse_dict(item[1])),
-                               mappers.items()))
+    struct_format = format_ + ''.join([field[1] for field in definition])
+    keys = list([field[0] for field in definition])
+    mappers = dict([(field[0], field[2]) for field in [field for field in definition if len(field) > 2]])
+    reverse_mappers = dict([(item[0], _reverse_dict(item[1])) for item in list(mappers.items())])
 
     def pack(**kwargs):
         unknown_fields = set(kwargs.keys()) - set(keys)
@@ -18,13 +16,13 @@ def create_struct_funcs(format_, definition):
             raise TypeError('Unknown field(s): {!r}'.format(unknown_fields))
         if len(missing_fields) > 0:
             raise TypeError('Missing field(s): {!r}'.format(missing_fields))
-        for key, mapper in mappers.items():
+        for key, mapper in list(mappers.items()):
             kwargs[key] = mapper[kwargs[key]]
-        return struct.pack(struct_format, *map(lambda key: kwargs[key], keys))
+        return struct.pack(struct_format, *[kwargs[key] for key in keys])
 
     def unpack(data):
-        result = dict(zip(keys, struct.unpack(struct_format, data)))
-        for key, mapper in reverse_mappers.items():
+        result = dict(list(zip(keys, struct.unpack(struct_format, data))))
+        for key, mapper in list(reverse_mappers.items()):
             result[key] = mapper[result[key]]
         return result
 
